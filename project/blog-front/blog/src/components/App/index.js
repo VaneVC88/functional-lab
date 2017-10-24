cf18e25027cd3a841aaaafcd4c48f8aea3c7cfc6
VaneVC88 <vane.vc.78@gmail.com>
2017-10-23 21:34:53 -0300

    projecto final

--- project/blog-front/blog/src/components/App/index.js
+++ project/blog-front/blog/src/components/App/index.js
@@ -4,6 +4,8 @@ import './App.css'
 import classnames from 'classnames'
 import { Link } from 'react-router-dom'
 
+import {getPosts} from '../../blog-api'
+
 class App extends Component {
   constructor (props) {
     super(props)
@@ -13,9 +15,12 @@ class App extends Component {
   }
 
   componentWillMount() {
-    //
-    // TÚ CÓDIGO VA AQUÍ
-    //
+    getPosts()
+    .then(resp => {
+      this.setState({
+        posts: resp,
+      })
+    })
   }
 
   render() {
--- project/blog-server/index.js
+++ project/blog-server/index.js
@@ -1,8 +1,12 @@
+// Archivo con datos (base de datos)
 const data = require('./data.json')
+// Dependencias del web service
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+// Nuestra APP (web services)
+const app = express()
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -12,15 +16,29 @@ const port = process.env.PORT || 8080
 const router = express.Router()
 
 router.get('/posts', (req, res) => {
-    // TÚ CÓDIGO VA AQUÍ
+    // Devolvemos todos los post :3
+    res.json(data)
 })
 
 router.get('/posts/:id', (req, res) => {
-    // TÚ CÓDIGO VA AQUÍ
+    const { id } = req.params
+    // 'data' necesitamos sacar el objeto 
+    // que haga match con su id con el dado en la url 
+    const post = data.filter((p) => {
+    	// Filtramos por esta condicion
+    	return p.id === parseInt(id, 10)
+    })[0]
+
+    // Comprobamos si es que hay algun dato filtrado
+    if (typeof post === 0){
+    	res.status(404).json({})
+    	return
+    }
+    // Devolvemos solo un post filtrado :3
+    res.json(post)
 })
 
 app.use('/api', router)
 
 app.listen(port)
 console.log('Magic happens on port ' + port)
-
